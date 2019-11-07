edb63a83e34a9090aa6a8333cd63f37bb44ffff5
kevprado <57108180+kevprado@users.noreply.github.com>
2019-11-07 15:44:03 -0500

    Update simulacionPanamericanos2019.py

--- simulacionPanamericanos2019/simulacionPanamericanos2019.py
+++ simulacionPanamericanos2019/simulacionPanamericanos2019.py
@@ -2,6 +2,28 @@ import tkinter
 import randomPaises
 
 #PRINCIPAL
+# -*- coding: utf-8 -*-
+import tkinter as tk
+ventana=tk.Tk()
+ventana.title("")
+
+#AnchoxAlto
+ventana.geometry("700x600")
+ventana.configure(background="pale goldenrod")
+
+
+
+
+etiqueta1=tk.Label(ventana,text="Paises:",bg="pale goldenrod",fg="black")
+etiqueta1.pack(padx=20,pady=20,side=tk.LEFT)
+
+image=tk.PhotoImage(file="1.gif")
+image=image.subsample(1,1)
+label=tk.Label(image=image)
+label.pack()
+
+
+ventana.mainloop()
 
 lista = randomPaises.rand(10)
 
