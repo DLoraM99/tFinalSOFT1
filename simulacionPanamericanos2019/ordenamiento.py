d9e3a93d175c8b2c37fa4761b28042908b77e60a
DLoraM99 <57108078+DLoraM99@users.noreply.github.com>
2019-11-07 13:35:59 -0500

    Create ordenamiento.py

---
+++ simulacionPanamericanos2019/ordenamiento.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+
+"""
+    El módulo ordena los elementos de una matriz de mayor a menor o de menor a mayor.
+"""
+
+
+#metadata
+__author__="Diego Lora"
+__copyrihgt__="Open source"
+__version__="v. 1.0"
+__mail__="diego.lora@usil.pe"
+__status__="Estudiante"
+
+
+#funciones
+def bubble(lista, tipo) :
+    """ funcion que realiza Buble Sort y ordena  """
+    
+    if (tipo=="menor") :
+      adad
+      
+    else :
+      adada
+
+    return lista
